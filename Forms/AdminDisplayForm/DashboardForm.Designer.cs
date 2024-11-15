6718323e01538641179f17f7aaef6fb8762867d8
chheng <chheng@gmail.com>
2024-11-16 00:22:29 +0700

    add class

--- Forms/AdminDisplayForm/DashboardForm.Designer.cs
+++ Forms/AdminDisplayForm/DashboardForm.Designer.cs
@@ -28,45 +28,225 @@
         /// </summary>
         private void InitializeComponent()
         {
-            labelDashboard = new Label();
             formsPlot1 = new ScottPlot.FormsPlot();
+            panel1 = new Panel();
+            label2 = new Label();
+            label1 = new Label();
+            panel2 = new Panel();
+            label3 = new Label();
+            label4 = new Label();
+            panel3 = new Panel();
+            label5 = new Label();
+            label6 = new Label();
+            panel4 = new Panel();
+            label7 = new Label();
+            label8 = new Label();
+            formsPlot2 = new ScottPlot.FormsPlot();
+            label9 = new Label();
+            label10 = new Label();
+            panel1.SuspendLayout();
+            panel2.SuspendLayout();
+            panel3.SuspendLayout();
+            panel4.SuspendLayout();
             SuspendLayout();
-            // 
-            // labelDashboard
-            // 
-            labelDashboard.AutoSize = true;
-            labelDashboard.Location = new Point(255, 50);
-            labelDashboard.Name = "labelDashboard";
-            labelDashboard.Size = new Size(64, 15);
-            labelDashboard.TabIndex = 0;
-            labelDashboard.Text = "Dashboard";
             // 
             // formsPlot1
             // 
-            formsPlot1.Location = new Point(109, 92);
+            formsPlot1.Location = new Point(614, 417);
             formsPlot1.Margin = new Padding(4, 3, 4, 3);
             formsPlot1.Name = "formsPlot1";
             formsPlot1.Size = new Size(450, 286);
             formsPlot1.TabIndex = 1;
+            // 
+            // panel1
+            // 
+            panel1.Controls.Add(label2);
+            panel1.Controls.Add(label1);
+            panel1.Location = new Point(12, 25);
+            panel1.Name = "panel1";
+            panel1.Size = new Size(249, 133);
+            panel1.TabIndex = 2;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Font = new Font("Sitka Small Semibold", 18F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label2.Location = new Point(184, 12);
+            label2.Name = "label2";
+            label2.Size = new Size(51, 35);
+            label2.TabIndex = 4;
+            label2.Text = "111";
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Sitka Small Semibold", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label1.Location = new Point(157, 99);
+            label1.Name = "label1";
+            label1.Size = new Size(69, 23);
+            label1.TabIndex = 3;
+            label1.Text = "All Use";
+            // 
+            // panel2
+            // 
+            panel2.Controls.Add(label3);
+            panel2.Controls.Add(label4);
+            panel2.Location = new Point(279, 25);
+            panel2.Name = "panel2";
+            panel2.Size = new Size(249, 133);
+            panel2.TabIndex = 3;
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Font = new Font("Sitka Small Semibold", 18F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label3.Location = new Point(192, 12);
+            label3.Name = "label3";
+            label3.Size = new Size(51, 35);
+            label3.TabIndex = 7;
+            label3.Text = "111";
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Font = new Font("Sitka Small Semibold", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label4.Location = new Point(143, 99);
+            label4.Name = "label4";
+            label4.Size = new Size(100, 23);
+            label4.TabIndex = 3;
+            label4.Text = "Today Sale";
+            // 
+            // panel3
+            // 
+            panel3.Controls.Add(label5);
+            panel3.Controls.Add(label6);
+            panel3.Location = new Point(547, 25);
+            panel3.Name = "panel3";
+            panel3.Size = new Size(249, 133);
+            panel3.TabIndex = 5;
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.Font = new Font("Sitka Small Semibold", 18F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label5.Location = new Point(195, 12);
+            label5.Name = "label5";
+            label5.Size = new Size(51, 35);
+            label5.TabIndex = 8;
+            label5.Text = "111";
+            // 
+            // label6
+            // 
+            label6.AutoSize = true;
+            label6.Font = new Font("Sitka Small Semibold", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label6.Location = new Point(120, 99);
+            label6.Name = "label6";
+            label6.Size = new Size(126, 23);
+            label6.TabIndex = 3;
+            label6.Text = "Today Income";
+            // 
+            // panel4
+            // 
+            panel4.Controls.Add(label7);
+            panel4.Controls.Add(label8);
+            panel4.Location = new Point(815, 25);
+            panel4.Name = "panel4";
+            panel4.Size = new Size(249, 133);
+            panel4.TabIndex = 5;
+            // 
+            // label7
+            // 
+            label7.AutoSize = true;
+            label7.Font = new Font("Sitka Small Semibold", 18F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label7.Location = new Point(195, 12);
+            label7.Name = "label7";
+            label7.Size = new Size(51, 35);
+            label7.TabIndex = 9;
+            label7.Text = "111";
+            // 
+            // label8
+            // 
+            label8.AutoSize = true;
+            label8.Font = new Font("Sitka Small Semibold", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label8.Location = new Point(100, 99);
+            label8.Name = "label8";
+            label8.Size = new Size(146, 23);
+            label8.TabIndex = 3;
+            label8.Text = "Today Customer";
+            // 
+            // formsPlot2
+            // 
+            formsPlot2.Location = new Point(78, 417);
+            formsPlot2.Margin = new Padding(4, 3, 4, 3);
+            formsPlot2.Name = "formsPlot2";
+            formsPlot2.Size = new Size(450, 286);
+            formsPlot2.TabIndex = 6;
+            // 
+            // label9
+            // 
+            label9.AutoSize = true;
+            label9.Font = new Font("Sitka Small Semibold", 14F, FontStyle.Bold);
+            label9.Location = new Point(115, 374);
+            label9.Name = "label9";
+            label9.Size = new Size(132, 28);
+            label9.TabIndex = 7;
+            label9.Text = "Weekly Sale";
+            // 
+            // label10
+            // 
+            label10.AutoSize = true;
+            label10.Font = new Font("Sitka Small Semibold", 14F, FontStyle.Bold);
+            label10.Location = new Point(661, 374);
+            label10.Name = "label10";
+            label10.Size = new Size(287, 28);
+            label10.TabIndex = 8;
+            label10.Text = "Today VS Yesterday Income";
             // 
             // DashboardForm
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(1092, 802);
+            Controls.Add(label10);
+            Controls.Add(label9);
+            Controls.Add(formsPlot2);
+            Controls.Add(panel3);
+            Controls.Add(panel4);
+            Controls.Add(panel2);
+            Controls.Add(panel1);
             Controls.Add(formsPlot1);
-            Controls.Add(labelDashboard);
             FormBorderStyle = FormBorderStyle.None;
             Name = "DashboardForm";
             Text = "DashboardForm";
             Load += DashboardForm_Load;
+            panel1.ResumeLayout(false);
+            panel1.PerformLayout();
+            panel2.ResumeLayout(false);
+            panel2.PerformLayout();
+            panel3.ResumeLayout(false);
+            panel3.PerformLayout();
+            panel4.ResumeLayout(false);
+            panel4.PerformLayout();
             ResumeLayout(false);
             PerformLayout();
         }
 
         #endregion
-
-        private Label labelDashboard;
         private ScottPlot.FormsPlot formsPlot1;
+        private Panel panel1;
+        private Label label2;
+        private Label label1;
+        private Panel panel2;
+        private Label label4;
+        private Panel panel3;
+        private Label label6;
+        private Panel panel4;
+        private Label label8;
+        private ScottPlot.FormsPlot formsPlot2;
+        private Label label3;
+        private Label label5;
+        private Label label7;
+        private Label label9;
+        private Label label10;
     }
 }
