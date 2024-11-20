5e1a1651d506e22a22c992687a7601d54338a37c
chheng <chheng@gmail.com>
2024-11-20 18:42:07 +0700

    add new file and modifies form

--- Forms/AdminDisplayForm/ProductsAddForm.Designer.cs
+++ Forms/AdminDisplayForm/ProductsAddForm.Designer.cs
@@ -28,12 +28,212 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "ProductsAddForm";
+            button2 = new Button();
+            textBox6 = new TextBox();
+            textBox5 = new TextBox();
+            btnUploadPhoto = new Button();
+            picStaff = new PictureBox();
+            labelCategoryID = new Label();
+            labelDescription = new Label();
+            textBox3 = new TextBox();
+            textBox2 = new TextBox();
+            labelProductName = new Label();
+            labelPrice = new Label();
+            button1 = new Button();
+            labelProductID = new Label();
+            textBox1 = new TextBox();
+            button3 = new Button();
+            ((System.ComponentModel.ISupportInitialize)picStaff).BeginInit();
+            SuspendLayout();
+            // 
+            // button2
+            // 
+            button2.Font = new Font("Microsoft Sans Serif", 14F, FontStyle.Bold);
+            button2.Location = new Point(335, 390);
+            button2.Name = "button2";
+            button2.Size = new Size(121, 49);
+            button2.TabIndex = 83;
+            button2.Text = "Insert";
+            button2.UseVisualStyleBackColor = true;
+            // 
+            // textBox6
+            // 
+            textBox6.Font = new Font("Sitka Small", 12F, FontStyle.Bold);
+            textBox6.Location = new Point(395, 32);
+            textBox6.Name = "textBox6";
+            textBox6.ReadOnly = true;
+            textBox6.Size = new Size(217, 28);
+            textBox6.TabIndex = 78;
+            // 
+            // textBox5
+            // 
+            textBox5.Font = new Font("Sitka Small", 12F, FontStyle.Bold);
+            textBox5.Location = new Point(172, 217);
+            textBox5.Name = "textBox5";
+            textBox5.Size = new Size(217, 28);
+            textBox5.TabIndex = 77;
+            // 
+            // btnUploadPhoto
+            // 
+            btnUploadPhoto.Font = new Font("Sitka Small", 11F, FontStyle.Bold);
+            btnUploadPhoto.Location = new Point(633, 215);
+            btnUploadPhoto.Name = "btnUploadPhoto";
+            btnUploadPhoto.Size = new Size(145, 30);
+            btnUploadPhoto.TabIndex = 76;
+            btnUploadPhoto.Text = "Upload Photo";
+            btnUploadPhoto.UseVisualStyleBackColor = true;
+            // 
+            // picStaff
+            // 
+            picStaff.BorderStyle = BorderStyle.FixedSingle;
+            picStaff.Location = new Point(633, 12);
+            picStaff.Name = "picStaff";
+            picStaff.Size = new Size(145, 185);
+            picStaff.TabIndex = 75;
+            picStaff.TabStop = false;
+            // 
+            // labelCategoryID
+            // 
+            labelCategoryID.AutoSize = true;
+            labelCategoryID.Font = new Font("Sitka Small", 12F, FontStyle.Bold);
+            labelCategoryID.Location = new Point(279, 32);
+            labelCategoryID.Name = "labelCategoryID";
+            labelCategoryID.Size = new Size(110, 23);
+            labelCategoryID.TabIndex = 71;
+            labelCategoryID.Text = "Category ID";
+            // 
+            // labelDescription
+            // 
+            labelDescription.AutoSize = true;
+            labelDescription.Font = new Font("Sitka Small", 12F, FontStyle.Bold);
+            labelDescription.Location = new Point(23, 222);
+            labelDescription.Name = "labelDescription";
+            labelDescription.Size = new Size(108, 23);
+            labelDescription.TabIndex = 70;
+            labelDescription.Text = "Description";
+            // 
+            // textBox3
+            // 
+            textBox3.Font = new Font("Sitka Small", 12F, FontStyle.Bold);
+            textBox3.Location = new Point(105, 156);
+            textBox3.Name = "textBox3";
+            textBox3.Size = new Size(158, 28);
+            textBox3.TabIndex = 68;
+            // 
+            // textBox2
+            // 
+            textBox2.Font = new Font("Sitka Small", 12F, FontStyle.Bold);
+            textBox2.Location = new Point(172, 101);
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(217, 28);
+            textBox2.TabIndex = 67;
+            // 
+            // labelProductName
+            // 
+            labelProductName.AutoSize = true;
+            labelProductName.Font = new Font("Sitka Small", 12F, FontStyle.Bold);
+            labelProductName.Location = new Point(23, 106);
+            labelProductName.Name = "labelProductName";
+            labelProductName.Size = new Size(129, 23);
+            labelProductName.TabIndex = 66;
+            labelProductName.Text = "Product Name";
+            // 
+            // labelPrice
+            // 
+            labelPrice.AutoSize = true;
+            labelPrice.Font = new Font("Sitka Small", 12F, FontStyle.Bold);
+            labelPrice.Location = new Point(23, 161);
+            labelPrice.Name = "labelPrice";
+            labelPrice.Size = new Size(52, 23);
+            labelPrice.TabIndex = 65;
+            labelPrice.Text = "Price";
+            // 
+            // button1
+            // 
+            button1.Font = new Font("Microsoft Sans Serif", 14F, FontStyle.Bold);
+            button1.Location = new Point(38, 390);
+            button1.Name = "button1";
+            button1.Size = new Size(121, 49);
+            button1.TabIndex = 64;
+            button1.Text = "Clear";
+            button1.UseVisualStyleBackColor = true;
+            // 
+            // labelProductID
+            // 
+            labelProductID.AutoSize = true;
+            labelProductID.Font = new Font("Sitka Small", 12F, FontStyle.Bold);
+            labelProductID.Location = new Point(23, 32);
+            labelProductID.Name = "labelProductID";
+            labelProductID.Size = new Size(101, 23);
+            labelProductID.TabIndex = 63;
+            labelProductID.Text = "Product ID";
+            // 
+            // textBox1
+            // 
+            textBox1.Font = new Font("Sitka Small", 12F, FontStyle.Bold);
+            textBox1.Location = new Point(105, 27);
+            textBox1.Name = "textBox1";
+            textBox1.ReadOnly = true;
+            textBox1.Size = new Size(133, 28);
+            textBox1.TabIndex = 62;
+            // 
+            // button3
+            // 
+            button3.Font = new Font("Microsoft Sans Serif", 14F, FontStyle.Bold);
+            button3.Location = new Point(607, 390);
+            button3.Name = "button3";
+            button3.Size = new Size(121, 49);
+            button3.TabIndex = 84;
+            button3.Text = "Update";
+            button3.UseVisualStyleBackColor = true;
+            // 
+            // ProductsAddForm
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(button3);
+            Controls.Add(button2);
+            Controls.Add(textBox6);
+            Controls.Add(textBox5);
+            Controls.Add(btnUploadPhoto);
+            Controls.Add(picStaff);
+            Controls.Add(labelCategoryID);
+            Controls.Add(labelDescription);
+            Controls.Add(textBox3);
+            Controls.Add(textBox2);
+            Controls.Add(labelProductName);
+            Controls.Add(labelPrice);
+            Controls.Add(button1);
+            Controls.Add(labelProductID);
+            Controls.Add(textBox1);
+            Name = "ProductsAddForm";
+            Text = "ProductsAddForm";
+            ((System.ComponentModel.ISupportInitialize)picStaff).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+        private Button button2;
+        private TextBox textBox8;
+        private TextBox textBox7;
+        private TextBox textBox6;
+        private TextBox textBox5;
+        private Button btnUploadPhoto;
+        private PictureBox picStaff;
+        private Label labelPersonalNumber;
+        private Label labelContactNumber;
+        private Label labelStaffAddress;
+        private Label labelDescription;
+        private Label labelCategoryID;
+        private TextBox textBox3;
+        private TextBox textBox2;
+        private Label labelProductName;
+        private Label labelPrice;
+        private Button button1;
+        private Label labelProductID;
+        private TextBox textBox1;
+        private Button button3;
     }
 }
--- Forms/AdminDisplayForm/StaffAddForm.Designer.cs
+++ Forms/AdminDisplayForm/StaffAddForm.Designer.cs
@@ -28,19 +28,305 @@
         /// </summary>
         private void InitializeComponent()
         {
+            textBox1 = new TextBox();
+            labelStaffID = new Label();
+            button1 = new Button();
+            labelGender = new Label();
+            labelStaffName = new Label();
+            textBox2 = new TextBox();
+            textBox3 = new TextBox();
+            labelDOB = new Label();
+            labelStaffPosition = new Label();
+            labelStaffAddress = new Label();
+            labelContactNumber = new Label();
+            labelPersonalNumber = new Label();
+            labelHiredDate = new Label();
+            picStaff = new PictureBox();
+            btnUploadPhoto = new Button();
+            textBox5 = new TextBox();
+            textBox6 = new TextBox();
+            textBox7 = new TextBox();
+            textBox8 = new TextBox();
+            dateTimePicker1 = new DateTimePicker();
+            dateTimePicker2 = new DateTimePicker();
+            button2 = new Button();
+            button3 = new Button();
+            ((System.ComponentModel.ISupportInitialize)picStaff).BeginInit();
             SuspendLayout();
+            // 
+            // textBox1
+            // 
+            textBox1.Font = new Font("Sitka Small", 12F, FontStyle.Bold);
+            textBox1.Location = new Point(94, 27);
+            textBox1.Name = "textBox1";
+            textBox1.ReadOnly = true;
+            textBox1.Size = new Size(133, 28);
+            textBox1.TabIndex = 0;
+            // 
+            // labelStaffID
+            // 
+            labelStaffID.AutoSize = true;
+            labelStaffID.Font = new Font("Sitka Small", 12F, FontStyle.Bold);
+            labelStaffID.Location = new Point(12, 32);
+            labelStaffID.Name = "labelStaffID";
+            labelStaffID.Size = new Size(76, 23);
+            labelStaffID.TabIndex = 1;
+            labelStaffID.Text = "Staff ID";
+            // 
+            // button1
+            // 
+            button1.Font = new Font("Microsoft Sans Serif", 14F, FontStyle.Bold);
+            button1.Location = new Point(27, 390);
+            button1.Name = "button1";
+            button1.Size = new Size(121, 49);
+            button1.TabIndex = 2;
+            button1.Text = "Clear";
+            button1.UseVisualStyleBackColor = true;
+            // 
+            // labelGender
+            // 
+            labelGender.AutoSize = true;
+            labelGender.Font = new Font("Sitka Small", 12F, FontStyle.Bold);
+            labelGender.Location = new Point(13, 89);
+            labelGender.Name = "labelGender";
+            labelGender.Size = new Size(70, 23);
+            labelGender.TabIndex = 3;
+            labelGender.Text = "Gender";
+            // 
+            // labelStaffName
+            // 
+            labelStaffName.AutoSize = true;
+            labelStaffName.Font = new Font("Sitka Small", 12F, FontStyle.Bold);
+            labelStaffName.Location = new Point(245, 34);
+            labelStaffName.Name = "labelStaffName";
+            labelStaffName.Size = new Size(104, 23);
+            labelStaffName.TabIndex = 4;
+            labelStaffName.Text = "Staff Name";
+            // 
+            // textBox2
+            // 
+            textBox2.Font = new Font("Sitka Small", 12F, FontStyle.Bold);
+            textBox2.Location = new Point(370, 29);
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(217, 28);
+            textBox2.TabIndex = 5;
+            // 
+            // textBox3
+            // 
+            textBox3.Font = new Font("Sitka Small", 12F, FontStyle.Bold);
+            textBox3.Location = new Point(89, 84);
+            textBox3.Name = "textBox3";
+            textBox3.Size = new Size(158, 28);
+            textBox3.TabIndex = 6;
+            // 
+            // labelDOB
+            // 
+            labelDOB.AutoSize = true;
+            labelDOB.Font = new Font("Sitka Small", 12F, FontStyle.Bold);
+            labelDOB.Location = new Point(13, 144);
+            labelDOB.Name = "labelDOB";
+            labelDOB.Size = new Size(127, 23);
+            labelDOB.TabIndex = 7;
+            labelDOB.Text = "Date Of Birth ";
+            // 
+            // labelStaffPosition
+            // 
+            labelStaffPosition.AutoSize = true;
+            labelStaffPosition.Font = new Font("Sitka Small", 12F, FontStyle.Bold);
+            labelStaffPosition.Location = new Point(255, 89);
+            labelStaffPosition.Name = "labelStaffPosition";
+            labelStaffPosition.Size = new Size(126, 23);
+            labelStaffPosition.TabIndex = 8;
+            labelStaffPosition.Text = "Staff Position";
+            // 
+            // labelStaffAddress
+            // 
+            labelStaffAddress.AutoSize = true;
+            labelStaffAddress.Font = new Font("Sitka Small", 12F, FontStyle.Bold);
+            labelStaffAddress.Location = new Point(12, 186);
+            labelStaffAddress.Name = "labelStaffAddress";
+            labelStaffAddress.Size = new Size(125, 23);
+            labelStaffAddress.TabIndex = 9;
+            labelStaffAddress.Text = "Staff Address";
+            // 
+            // labelContactNumber
+            // 
+            labelContactNumber.AutoSize = true;
+            labelContactNumber.Font = new Font("Sitka Small", 12F, FontStyle.Bold);
+            labelContactNumber.Location = new Point(13, 236);
+            labelContactNumber.Name = "labelContactNumber";
+            labelContactNumber.Size = new Size(146, 23);
+            labelContactNumber.TabIndex = 10;
+            labelContactNumber.Text = "Contact Number";
+            // 
+            // labelPersonalNumber
+            // 
+            labelPersonalNumber.AutoSize = true;
+            labelPersonalNumber.Font = new Font("Sitka Small", 12F, FontStyle.Bold);
+            labelPersonalNumber.Location = new Point(12, 281);
+            labelPersonalNumber.Name = "labelPersonalNumber";
+            labelPersonalNumber.Size = new Size(156, 23);
+            labelPersonalNumber.TabIndex = 11;
+            labelPersonalNumber.Text = "Personal Number";
+            // 
+            // labelHiredDate
+            // 
+            labelHiredDate.AutoSize = true;
+            labelHiredDate.Font = new Font("Sitka Small", 12F, FontStyle.Bold);
+            labelHiredDate.Location = new Point(11, 332);
+            labelHiredDate.Name = "labelHiredDate";
+            labelHiredDate.Size = new Size(102, 23);
+            labelHiredDate.TabIndex = 12;
+            labelHiredDate.Text = "Hired Date";
+            // 
+            // picStaff
+            // 
+            picStaff.BorderStyle = BorderStyle.FixedSingle;
+            picStaff.Location = new Point(622, 12);
+            picStaff.Name = "picStaff";
+            picStaff.Size = new Size(145, 185);
+            picStaff.TabIndex = 13;
+            picStaff.TabStop = false;
+            // 
+            // btnUploadPhoto
+            // 
+            btnUploadPhoto.Font = new Font("Sitka Small", 11F, FontStyle.Bold);
+            btnUploadPhoto.Location = new Point(622, 215);
+            btnUploadPhoto.Name = "btnUploadPhoto";
+            btnUploadPhoto.Size = new Size(145, 30);
+            btnUploadPhoto.TabIndex = 51;
+            btnUploadPhoto.Text = "Upload Photo";
+            btnUploadPhoto.UseVisualStyleBackColor = true;
+            // 
+            // textBox5
+            // 
+            textBox5.Font = new Font("Sitka Small", 12F, FontStyle.Bold);
+            textBox5.Location = new Point(387, 84);
+            textBox5.Name = "textBox5";
+            textBox5.Size = new Size(217, 28);
+            textBox5.TabIndex = 53;
+            // 
+            // textBox6
+            // 
+            textBox6.Font = new Font("Sitka Small", 12F, FontStyle.Bold);
+            textBox6.Location = new Point(153, 181);
+            textBox6.Name = "textBox6";
+            textBox6.Size = new Size(217, 28);
+            textBox6.TabIndex = 54;
+            // 
+            // textBox7
+            // 
+            textBox7.Font = new Font("Sitka Small", 12F, FontStyle.Bold);
+            textBox7.Location = new Point(176, 231);
+            textBox7.Name = "textBox7";
+            textBox7.Size = new Size(217, 28);
+            textBox7.TabIndex = 55;
+            // 
+            // textBox8
+            // 
+            textBox8.Font = new Font("Sitka Small", 12F, FontStyle.Bold);
+            textBox8.Location = new Point(187, 276);
+            textBox8.Name = "textBox8";
+            textBox8.Size = new Size(217, 28);
+            textBox8.TabIndex = 56;
+            // 
+            // dateTimePicker1
+            // 
+            dateTimePicker1.Font = new Font("Sitka Small", 12F, FontStyle.Bold);
+            dateTimePicker1.Location = new Point(132, 327);
+            dateTimePicker1.Name = "dateTimePicker1";
+            dateTimePicker1.Size = new Size(313, 28);
+            dateTimePicker1.TabIndex = 58;
+            // 
+            // dateTimePicker2
+            // 
+            dateTimePicker2.Font = new Font("Sitka Small", 12F, FontStyle.Bold);
+            dateTimePicker2.Location = new Point(153, 139);
+            dateTimePicker2.Name = "dateTimePicker2";
+            dateTimePicker2.Size = new Size(313, 28);
+            dateTimePicker2.TabIndex = 59;
+            // 
+            // button2
+            // 
+            button2.Font = new Font("Microsoft Sans Serif", 14F, FontStyle.Bold);
+            button2.Location = new Point(324, 390);
+            button2.Name = "button2";
+            button2.Size = new Size(121, 49);
+            button2.TabIndex = 60;
+            button2.Text = "Insert";
+            button2.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            button3.Font = new Font("Microsoft Sans Serif", 14F, FontStyle.Bold);
+            button3.Location = new Point(596, 390);
+            button3.Name = "button3";
+            button3.Size = new Size(121, 49);
+            button3.TabIndex = 61;
+            button3.Text = "Update";
+            button3.UseVisualStyleBackColor = true;
             // 
             // StaffAddForm
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(800, 450);
+            Controls.Add(button3);
+            Controls.Add(button2);
+            Controls.Add(dateTimePicker2);
+            Controls.Add(dateTimePicker1);
+            Controls.Add(textBox8);
+            Controls.Add(textBox7);
+            Controls.Add(textBox6);
+            Controls.Add(textBox5);
+            Controls.Add(btnUploadPhoto);
+            Controls.Add(picStaff);
+            Controls.Add(labelHiredDate);
+            Controls.Add(labelPersonalNumber);
+            Controls.Add(labelContactNumber);
+            Controls.Add(labelStaffAddress);
+            Controls.Add(labelStaffPosition);
+            Controls.Add(labelDOB);
+            Controls.Add(textBox3);
+            Controls.Add(textBox2);
+            Controls.Add(labelStaffName);
+            Controls.Add(labelGender);
+            Controls.Add(button1);
+            Controls.Add(labelStaffID);
+            Controls.Add(textBox1);
+            MaximumSize = new Size(816, 489);
             Name = "StaffAddForm";
             StartPosition = FormStartPosition.CenterScreen;
             Text = "StaffAddForm";
+            ((System.ComponentModel.ISupportInitialize)picStaff).EndInit();
             ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private TextBox textBox1;
+        private Label labelStaffID;
+        private Button button1;
+        private Label labelGender;
+        private Label labelStaffName;
+        private TextBox textBox2;
+        private TextBox textBox3;
+        private Label labelDOB;
+        private Label labelStaffPosition;
+        private Label labelStaffAddress;
+        private Label labelContactNumber;
+        private Label labelPersonalNumber;
+        private Label labelHiredDate;
+        private PictureBox picStaff;
+        private Button btnUploadPhoto;
+        private TextBox textBox5;
+        private TextBox textBox6;
+        private TextBox textBox7;
+        private TextBox textBox8;
+        private DateTimePicker dateTimePicker1;
+        private DateTimePicker dateTimePicker2;
+        private Button button2;
+        private Button button3;
     }
 }
